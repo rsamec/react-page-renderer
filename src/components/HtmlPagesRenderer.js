c0520085f2c3a2d25de51e4d7ad0392c22db94be
rsamec <roman.samec2@gmail.com>
2015-06-02 20:37:14 +0200

    page renderer - partial support for two way data binding

--- src/components/HtmlPagesRenderer.js
+++ src/components/HtmlPagesRenderer.js
@@ -1,6 +1,12 @@
-var React = require('react');
+import React from 'react';
+import _ from 'underscore';
+import BindToMixin from 'react-binding';
+import {numberConverter} from '../utilities/converters.js';
+
+
 var transformToPages = require('../utilities/transformToPages');
 var standardPageSizes = require('./standardPageSizes');
+
 
 var HtmlPage = React.createClass({
 	render: function () {
@@ -47,17 +53,62 @@ var HtmlPage = React.createClass({
 });
 
 var HtmlPagesRenderer = React.createClass({
+	mixins:[BindToMixin],
+	applyBinding:function(box, dataContext){
+		var ref = function (obj, str) {
+			return str.split(".").reduce(function(o, x) { return o === undefined?undefined:o[x] }, obj);
+		}
+		//apply binding
+		for (var propName in box){
+			var prop = box[propName];
+			//TODO: better test - it is a binding object?
+			if (_.isObject(prop) && !!prop.Path){
+				if (propName === 'onAdd') {
+					var selfBinding = this.bindArrayTo(dataContext,prop.Path);
+					box.onClick = function(e){ selfBinding.add()}
+				}
+				else if (propName === 'onRemove'){
+					var selfBinding = this.props.arrayContext;
+					box.onClick = function(e){  selfBinding.remove(dataContext.value);}
+				}
+				else if (prop.Mode === "TwoWay") {
+					//two-way binding
+					var converter;
+					if (!!prop.Converter) {
+						converter = new numberConverter();
+
+					}
+
+					box.valueLink = converter!==undefined?this.bindTo(dataContext, prop.Path,converter):this.bindTo(dataContext,prop.Path);
+					box.value = undefined;
+
+					//error - one way binding
+					var error = ref(this.props.errors,prop.Path);
+					if (error !== undefined) {
+						box.help = error.ErrorMessage;
+						box.bsStyle = error.HasErrors ? 'error' : '';
+					}
+				}
+				else{
+					//one-way binding
+					//box[propName] = this.bindTo(dataContext, prop.Path).value;
+				}
+			}
+		}
+
+	},
 	createComponent: function (box) {
 		var widget = this.props.widgets[box.elementName];
 		if (widget === undefined) return React.DOM.span(null, 'Component ' + box.elementName + ' is not register among widgets.');
 
+		if (this.props.dataContext !== undefined) this.applyBinding(box,this.props.dataContext);
 		var props = box;
 		return React.createElement(widget, props, box.content !== undefined ? React.DOM.span(null, box.content) : undefined);
 	},
 	render: function () {
 		
 		var pages = transformToPages(this.props.schema,this.props.data);
-
+		
 		return (
 			<div id="section-to-print">
 				{pages.map(function (page, i) {
